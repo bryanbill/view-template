8a18db22ab097974ecaf974f5748722662cd8c58
bryanbill <emobrya@gmail.com>
2024-06-08 19:00:08 +0300

    darr wasm bootstrap

---
+++ web/main.dart.js
@@ -0,0 +1,31 @@
+(async function () {
+    let dart2wasm_runtime;
+    let moduleInstance;
+    try {
+        const dartModulePromise = WebAssembly.compileStreaming(fetch('main.wasm'));
+        const imports = {};
+        dart2wasm_runtime = await import('./main.mjs');
+        moduleInstance = await dart2wasm_runtime.instantiate(dartModulePromise, imports);
+    } catch (exception) {
+        console.error(`Failed to fetch and instantiate wasm module: ${exception}`);
+        console.error('See https://dart.dev/web/wasm for more information.');
+        console.warn('Running Dart code in JavaScript');
+
+        runDartJs();
+        return;
+    }
+
+    if (moduleInstance) {
+        try {
+            await dart2wasm_runtime.invoke(moduleInstance);
+        } catch (exception) {
+            console.error(`Exception while invoking test: ${exception}`);
+        }
+    }
+})();
+
+function runDartJs() {
+    import('./main.js').then(module => {
+        console.log('Running Dart code in JavaScript on iOS');
+    });
+}
